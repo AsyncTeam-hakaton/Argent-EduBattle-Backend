3923d9d8d5206e4942962ed6feef84e27e3ffc11
Platon <platongimp96@gmail.com>
2026-09-17 09:44:41 +0500

    fix: init of models

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -2,4 +2,4 @@ from src.database.base import Base
 from src.database.models.rooms_models import Room, RoomParticipant
 from src.database.models.users_models import User
 
-__all__ = ["Base", "User", "Room", "RoomParticipant"]
+__all__ = ["Base", "Room", "RoomParticipant", "User"]
